2f005bc7d187051d102c193bdf641ac50f6c9da8
Allan-Victor <akibuuka50@gmail.com>
2024-06-02 21:24:34 +0300

    Removed Qodana workflow

--- backend/src/main/java/com/allancode/customer/Customer.java
+++ backend/src/main/java/com/allancode/customer/Customer.java
@@ -37,7 +37,6 @@ public class Customer {
             this.age = age;
         }
     public Customer(String name, String email, Integer age) {
-        this.id = id;
         this.name = name;
         this.email = email;
         this.age = age;
